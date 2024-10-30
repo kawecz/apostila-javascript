d76a6ed898b48befa3d31e1782e5cc2319e80d1b
Kawê Cezar <kawecezarjesus@gmail.com>
2024-10-30 18:03:15 -0500

    Desafio 23 - Dia da Mulher.

--- d23/script.js
+++ d23/script.js
@@ -1,15 +1,24 @@
 function desconto(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const nome = document.querySelector("#nome").value
-    const sexo = document.querySelector("#sexo").value
-    const resposta = document.querySelector(".res")
-    const produto = document.querySelector("#produto").value
+    const nome = document.querySelector("#nome").value;
+    const sexo = document.querySelector('input[name="sex"]:checked').value;
+    const resposta = document.querySelector(".res");
+    const produto = Number(document.querySelector("#produto").value);
 
-    if(sexo === "Masculino"){
-        const desconto = Number(produto) * 5 / 100
-        const novo_valor = Number(produto) - Number(desconto)
-       resposta.innerHTML = `<p>Bem vindo ao nosso site ${nome}</p>`
-       resposta.innerHTML = `<p>Aplicando um desconto de (5%) no seu produto</p>`
+
+
+    if (sexo === "Masculino") {
+        desconto = produto * 0.05;
+    } else if (sexo === "Feminino") {
+        desconto = produto * 0.13;
     }
+
+    const novo_valor = produto - desconto;
+
+    resposta.innerHTML = `
+        <p>Bem-vindo(a) ao nosso site, <strong>${nome}!</strong></p>
+        <p>Aplicando um desconto de <strong>${sexo === "Masculino" ? "5%" : "13%"}</strong> no seu produto <strong>(R$ ${produto.toFixed(2)})</strong></p>
+        <p>O novo valor será de: <strong>R$ ${novo_valor.toFixed(2)}</strong></p>
+    `;
 }
